fd2b7175f83627784247783baa11ad1895648285
piedrabryan <piedrabr@oregonstate.edu>
2024-03-18 12:36:52 -0700

    Deleted JSON files

--- cypress/e2e/spec.cy.js
+++
@@ -1,9 +0,0 @@
-describe('template spec', function() {
-    it('passes', function() {
-      cy.visit('https://example.cypress.io')
-      cy.contains("dblclick").click()
-  
-      // example.cypress.io/commands/actions
-      cy.url().should("include", "/commands/actions")
-    })
-  })
